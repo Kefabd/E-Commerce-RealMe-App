2ea305a7950d392c63c1510af8bb80e209fd37bf
abdou <abdoukefsi@gmail.com>
2024-01-11 14:16:54 +0100

    Finish2

--- pages/products/category.js
+++ pages/products/category.js
@@ -27,7 +27,7 @@ export default function Categories() {
       }
     };
     fetchData();
-  });
+  }, []);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
