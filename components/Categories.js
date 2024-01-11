12d5e1598090f7fbe6fe874adb2930382449cb15
abdou <abdoukefsi@gmail.com>
2024-01-11 09:30:10 +0100

    for getting the css

---
+++ components/Categories.js
@@ -0,0 +1,8 @@
+export default function Categories({categories}) {
+    return (
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 mt-6">
+          ccccccccccc
+        </div>
+      );
+    
+}
--- pages/products/category.js
+++ pages/products/category.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "../../components/Header";
+import Head from "next/head";
+import Footer from "../../components/Footer";
+import * as Realm from "realm-web";
+import Link from "next/link";
+
+
 
 
 export default function Categories() {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // add your Realm App Id to the .env.local file
+      const REALM_APP_ID = process.env.NEXT_PUBLIC_REALM_APP_ID;
+      const app = new Realm.App({ id: REALM_APP_ID });
+      const credentials = Realm.Credentials.anonymous();
+      try {
+        const user = await app.logIn(credentials);
+        const allCategories = await user.functions.getAllCategories();
+        setCategories(() => allCategories);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -12,13 +37,17 @@ export default function Categories() {
           </Head>
           <div className="bg-white w-full min-h-screen">
             <Header />
-            <Container>
-              <Category
-                category="Tech Wear"
-                //categoryCount={`${products.length} Products`}
-              />
-
-            </Container>
+            {categories.map((category) => (
+              <Link href={`/products/${category}`}>
+                <div key={category._id}>{category}</div>
+              </Link>
+              
+            )
+              
+             
+                
+            )}
+           
             <Footer />
           </div>
         </div>
